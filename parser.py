cc810465226ce1cfad3aa63682d478f32eb6b2c3
chaitanyanettem <chaitanyanettem@gmail.com>
2015-03-17 06:36:54 -0400

    Refactored code; Defined write_info_table_csv(); Defined prep_csv_string()

--- parser.py
+++ parser.py
@@ -67,6 +67,18 @@ class cik:
 		# f_info_table = open("info_table.xml","w")
 		# f_info_table.write(self.info_table)
 		# f_info_table.close()
+
+	def prep_csv_string(self, commaList):
+		
+		commaSeparated = []
+		for x in commaList:
+			if x is not None:
+				commaSeparated.append(x.text)
+			else:
+				commaSeparated.append('None')
+		commaSeparated = ",".join(commaSeparated)
+		return commaSeparated
+
 		
 	def write_primary_doc_csv(self):
 		# Although the spec lists a lot of information for primary_doc, a lot
@@ -85,25 +97,71 @@ class cik:
 		tableEntryTotal_tag = "".join([formData_tag, summaryPage_tag, namespace, "tableEntryTotal"])
 		tableValueTotal_tag = "".join([formData_tag, summaryPage_tag, namespace, "tableValueTotal"])
 		
-		periodOfReport = primary_doc_parse.find(periodOfReport_tag).text
-		tableEntryTotal = primary_doc_parse.find(tableEntryTotal_tag).text
-		tableValueTotal = primary_doc_parse.find(tableValueTotal_tag).text
-		commaSeparated = ",".join([periodOfReport, tableEntryTotal, tableValueTotal])
-		print commaSeparated
+		commaList = []
+		commaList.append(primary_doc_parse.find(periodOfReport_tag))
+		commaList.append(primary_doc_parse.find(tableEntryTotal_tag))
+		commaList.append(primary_doc_parse.find(tableValueTotal_tag))
 
+		commaSeparated = self.prep_csv_string(commaList)
+
+		print commaSeparated
 		f = open("primary_doc.csv","w")
 		f.write(commaSeparated)
 		f.close()
 
 	def write_info_table_csv(self):
 		# I'm getting all the information in each infoTable tag and writing to a csv file
-
+		
 		info_table_parse = etree.fromstring(self.info_table)
-		namespace = "{http://www.sec.gov/edgar/document/thirteenf/informationtable}"
 
+		namespace = "{http://www.sec.gov/edgar/document/thirteenf/informationtable}"
+		infoTable_tag = "".join([namespace, "infoTable"])
+		infoTable_tag2 = infoTable_tag + "/"
+		nameOfIssuer_tag = 	"".join([namespace, "nameOfIssuer"])
+		titleOfClass_tag = "".join([namespace, "titleOfClass"])
+		cusip_tag = "".join([namespace, "cusip"])
+		value_tag = "".join([namespace, "value"])
+		
+		shrsOrPrnAmt_tag = "".join([namespace, "shrsOrPrnAmt/"])
+		sshPrnamt_tag = "".join([shrsOrPrnAmt_tag, namespace, "sshPrnamt"])
+		sshPrnamtType_tag = "".join([shrsOrPrnAmt_tag, namespace, "sshPrnamtType"])
+		
+		putCall_tag = "".join([namespace, "putCall"])
+		investmentDiscretion_tag = "".join([namespace, "investmentDiscretion"])
+		otherManager_tag = "".join([namespace, "otherManager"])
+		
+		votingAuthority_tag = "".join([namespace, "votingAuthority/"])
+		sole_tag = "".join([votingAuthority_tag, namespace, "Sole"])
+		shared_tag = "".join([votingAuthority_tag, namespace, "Shared"])
+		none_tag = "".join([votingAuthority_tag, namespace, "None"])
+
+		f = open("info_table.csv","w")
+
+		for info_table in info_table_parse.findall(infoTable_tag):
+			
+			commaList = []
+			commaList.append(info_table.find(nameOfIssuer_tag))
+			commaList.append(info_table.find(titleOfClass_tag))
+			commaList.append(info_table.find(cusip_tag))
+			commaList.append(info_table.find(value_tag))
+			commaList.append(info_table.find(sshPrnamt_tag))
+			commaList.append(info_table.find(sshPrnamtType_tag))
+			commaList.append(info_table.find(putCall_tag))
+			commaList.append(info_table.find(investmentDiscretion_tag))
+			commaList.append(info_table.find(otherManager_tag))
+			commaList.append(info_table.find(sole_tag))
+			commaList.append(info_table.find(shared_tag))
+			commaList.append(info_table.find(none_tag))
+
+			commaSeparated = self.prep_csv_string(commaList)
+			print commaSeparated
+			f.write(commaSeparated+"\n")
+		
+		f.close()
 
 if __name__ == '__main__':
 	ticker = cik(sys.argv[1])
 	ticker.find_first_txt_link()
 	ticker.split_txt_file()
 	ticker.write_primary_doc_csv()
+	ticker.write_info_table_csv()
